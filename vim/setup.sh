ba16149b41faedc2313c86c065b6af70e9ab16c1
Johnny Hauser <johnnyhauser@gmail.com>
2016-07-14 13:53:57 -0500

    added vim node

--- vim/setup.sh
+++ vim/setup.sh
@@ -10,12 +10,13 @@ curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 cd ~/.vim/bundle
 git clone https://github.com/mxw/vim-jsx.git
-git clone git://github.com/digitaltoad/vim-pug.git
+git clone https://github.com/digitaltoad/vim-pug.git
 git clone https://github.com/gioele/vim-autoswap.git
 git clone https://github.com/Yggdroot/indentLine
 git clone https://github.com/othree/yajs.vim
 git clone https://github.com/othree/es.next.syntax.vim
 git clone https://github.com/elzr/vim-json
+git clone https://github.com/moll/vim-node.git
 # git clone https://github.com/bigfish/vim-js-context-coloring
 
 cp $dir/vimrc ~/
