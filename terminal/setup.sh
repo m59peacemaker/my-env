3ac1e688974c38688cb822d013fb0558940528ac
Johnny Hauser <johnnyhauser@gmail.com>
2017-03-01 22:49:53 -0600

    added vim-commentary, enable globstar

--- terminal/setup.sh
+++ terminal/setup.sh
@@ -8,4 +8,7 @@ echo "export PROMPT_DIRTRIM=3" >> ~/.bashrc
 # remove machine name from terminal
 echo "PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '" >> ~/.bashrc
 
+# make **/*.js match all depths
+shopt -s globstar
+
 $dir/gogh-custom-theme.sh
--- vim/setup.sh
+++ vim/setup.sh
@@ -9,14 +9,16 @@ mkdir -p ~/.vim/autoload ~/.vim/bundle
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 cd ~/.vim/bundle
-git clone https://github.com/mxw/vim-jsx.git
-git clone https://github.com/digitaltoad/vim-pug.git
-git clone https://github.com/gioele/vim-autoswap.git
-git clone https://github.com/Yggdroot/indentLine
-git clone https://github.com/othree/yajs.vim
 git clone https://github.com/othree/es.next.syntax.vim
+git clone https://github.com/Yggdroot/indentLine
+git clone https://github.com/gioele/vim-autoswap.git
+git clone https://github.com/tpope/vim-commentary
 git clone https://github.com/elzr/vim-json
+git clone https://github.com/mxw/vim-jsx.git
 git clone https://github.com/moll/vim-node.git
+git clone https://github.com/digitaltoad/vim-pug.git
+git clone https://github.com/othree/yajs.vim
+
 # git clone https://github.com/bigfish/vim-js-context-coloring
 
 cp $dir/vimrc ~/
